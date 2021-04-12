6d4b6ab1e64ea49a368ab50272725050a9a2f99a
sy12-glitch <ssahanays990@gmail.com>
2021-04-12 16:29:43 +0530

    server side

---
+++ Serverside/Serverside/src/main/java/com/application/entity/Product.java
@@ -0,0 +1,113 @@
+package com.application.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table(name = "product")
+
+
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private int quantity;
+	private String name;
+	private String brand;
+	private double price;
+	private String image;
+	private String description;
+	private float rating;
+	private int category_id;
+	
+	public Product(int id, int quantity, String name, String brand, double price, String image, String description,
+			float rating, int category_id) {
+		super();
+		this.id = id;
+		this.quantity = quantity;
+		this.name = name;
+		this.brand = brand;
+		this.price = price;
+		this.image = image;
+		this.description = description;
+		this.rating = rating;
+		this.category_id = category_id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public float getRating() {
+		return rating;
+	}
+
+	public void setRating(float rating) {
+		this.rating = rating;
+	}
+
+	public int getCategory_id() {
+		return category_id;
+	}
+
+	public void setCategory_id(int category_id) {
+		this.category_id = category_id;
+	}
+	
+	
+}
