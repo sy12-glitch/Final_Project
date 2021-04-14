daa37f51e8dea9bbcbf4dbabea9f41dc37e3b17e
Atitkumar <amachhirke6@gmail.com>
2021-04-14 16:37:33 +0530

    sign-up+login

--- online-shopping/src/sign-up/login/login.component.ts
+++ online-shopping/src/sign-up/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+  constructor(private router: Router) { }
+  username: string;
+  password: string;
+  
+    ngOnInit() {
+    }
+  
+    login() : void {
+      if(this.username == 'admin' && this.password == 'admin'){
+       this.router.navigate(["/"]);
+      }else {
+        alert("Invalid credentials");
+      }
+    }
+    }
+    
