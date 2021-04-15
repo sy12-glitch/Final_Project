e8a10e863104aeab06c4fcf0e7ebc745ff608e62
Atitkumar <amachhirke6@gmail.com>
2021-04-15 11:46:43 +0530

    Check the error...

--- online-shopping/src/sign-up/login/login.component.ts
+++ online-shopping/src/sign-up/login/login.component.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { EmailValidator, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { CustomerService } from '../service/customer.service';
 
 @Component({
   selector: 'app-login',
@@ -8,19 +11,25 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private router: Router) { }
-  username: string;
-  password: string;
-  
-    ngOnInit() {
+  constructor(private router: Router, 
+    private http: HttpClient, private customService:CustomerService) { }
+
+  loginForm: FormGroup;
+    ngOnInit(): void {
+      this.loginForm = new FormGroup({
+        username:new FormControl("",Validators.required),
+        password :new FormControl("",Validators.required)
+      });
     }
   
     login() : void {
-      if(this.username == 'admin' && this.password == 'admin'){
-       this.router.navigate(["/"]);
-      }else {
-        alert("Invalid credentials");
-      }
+      
+      console.log(this.loginForm.value);
+      this.customService.saveForm(this.loginForm.value)
+      .subscribe((res:any)=>{
+        console.log(res);
+        this.router.navigate(["home"]);
+      })
     }
-    }
+  }
     
