72e9d5d3d6eb27c46d94b55316f645bcedb78879
Atitkumar <amachhirke6@gmail.com>
2021-04-15 10:18:30 +0530

    update

--- online-shopping/src/sign-up/sign-up/sign-up.component.ts
+++ online-shopping/src/sign-up/sign-up/sign-up.component.ts
@@ -33,48 +33,8 @@ export class SignUpComponent implements OnInit {
   
       .subscribe((res:any)=>{
         console.log(res);
-        this.router.navigate(["login"]);
+        this.router.navigate(["sign-up/login"]);
       })
     }
   }
 
-// export class SignUpComponent {
-//   title = 'angularpopup';
-//   showModal: boolean;
-//   registerForm: FormGroup;
-//   submitted = false;
-//   constructor(private formBuilder: FormBuilder) { }
-//   show()
-//   {
-//     this.showModal = true; // Show-Hide Modal Check
-    
-//   }
-//   //Bootstrap Modal Close event
-//   hide()
-//   {
-//     this.showModal = false;
-//   }
-//   ngOnInit() {
-//     this.registerForm = this.formBuilder.group({
-//         email: ['', [Validators.required, Validators.email]],
-//         password: ['', [Validators.required, Validators.minLength(6)]],
-//         firstname: ['', [Validators.required, Validators.minLength(6)]],
-//         mobile: ['', [Validators.required, Validators.pattern(/^-?(0|[1-9]\d*)?$/), Validators.minLength(10)]]
-//     });
-// }
-// // convenience getter for easy access to form fields
-// get f() { return this.registerForm.controls; }
-// onSubmit() {
-//     this.submitted = true;
-//     // stop here if form is invalid
-//     if (this.registerForm.invalid) {
-//         return;
-//     }
-//     if(this.submitted)
-//     {
-//       this.showModal = false;
-//     }
-   
-// }
-  
-// }
