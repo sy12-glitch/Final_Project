f51cd2e110273219f69d7b34b806469a94ecfed2
Atitkumar <amachhirke6@gmail.com>
2021-04-19 08:00:13 +0530

    commit

--- online-shopping/src/sign-up/sign-up/sign-up.component.ts
+++ online-shopping/src/sign-up/sign-up/sign-up.component.ts
@@ -16,11 +16,11 @@ export class SignUpComponent implements OnInit {
 
   ngOnInit(): void {
     this.signForm = new FormGroup({
-      fname:new FormControl("",[Validators.required,Validators.minLength(3)]),
-      lname: new FormControl("", [Validators.required,Validators.minLength(3)]),// Validators.pattern('^[a-zA-Z]+(([,. -][a-zA-Z ])?[a-zA-Z]*)*$')]),
-      email: new FormControl("", [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$") ]),
-      password: new FormControl("", [Validators.required,Validators.minLength(6)]),//Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$^+=!*()@%&]).{6,10}$')]),
-      address: new FormControl("", [Validators.required, ]),//Validators.pattern('(\d{1,}) [a-zA-Z0-9\s]+(\.)? [a-zA-Z]+(\,)? [A-Z]{2} [0-9]{5,6}')]),
+      fname:new FormControl("Atit",[Validators.required,Validators.minLength(3)]),
+      lname: new FormControl("Mach", [Validators.required,Validators.minLength(3)]),// Validators.pattern('^[a-zA-Z]+(([,. -][a-zA-Z ])?[a-zA-Z]*)*$')]),
+      email: new FormControl("at@gmail.com", [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$") ]),
+      password: new FormControl("Atit@123", [Validators.required,Validators.minLength(6)]),//Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$^+=!*()@%&]).{6,10}$')]),
+      address: new FormControl("Nagpur", [Validators.required, ]),//Validators.pattern('(\d{1,}) [a-zA-Z0-9\s]+(\.)? [a-zA-Z]+(\,)? [A-Z]{2} [0-9]{5,6}')]),
       mobile: new FormControl("", [Validators.required,Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$"), Validators.minLength(10)]),//Validators.pattern('^[0][1-9]\d{9}$|^[1-9]\d{9}$')]),
       gender: new FormControl("", Validators.required),
       role: new FormControl("", Validators.required)
