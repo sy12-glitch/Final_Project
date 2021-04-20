960fd3fdbe617ab82f3721a30968c86c2e3e7720
Sweety Sweety <sweety.sweety@ibm.com>
2021-04-20 11:52:26 +0530

    admin

--- online-shopping/src/app.routing.ts
+++ online-shopping/src/app.routing.ts
@@ -31,6 +31,7 @@ const routes:Routes = [
     {path: 'list', component: ListProductComponent},
     {path:'add-product', component: ProductCreateComponent},
     {path:'list-product', component: ListProductComponent},
+ 
    
 
    
