a566b4d55ced612da3aa6c2bc1a86460b1780f9f
sy12-glitch <ssahanays990@gmail.com>
2021-04-23 18:21:26 +0530

    invoice

--- online-shopping/src/my-orders/my-orders.component.ts
+++ online-shopping/src/my-orders/my-orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { UserdetailsService } from 'src/Services/userdetails.service';
 
 @Component({
@@ -9,11 +10,13 @@ import { UserdetailsService } from 'src/Services/userdetails.service';
 export class MyOrdersComponent implements OnInit {
  
     images = [1,2,3].map(() => `https://visitclearwaterflorida.com/wp-content/uploads/2017/05/women-shopping.jpg`);
+  router: any;
     
 
-  constructor(private userdetailsservice:UserdetailsService) { }
+  constructor(private userdetailsservice:UserdetailsService, router: Router) { }
 
   ngOnInit(): void {
+    this.logout();
   }
 
 logout(){
@@ -24,14 +27,14 @@ logout(){
   .subscribe(
     data =>{
     console.log("logged out");
-      console.log(data);
+    console.log(data);
      if(data==true){
        localStorage.removeItem('userlogindetails');
        let userstring1=localStorage.getItem('userlogindetails');
        const userlogin1 = JSON.parse(userstring1);
        console.log(userlogin1);
+       this.router.navigate(["/home-page"]);
      }
-    
   },
   error=>{
     console.log("exception occured");
